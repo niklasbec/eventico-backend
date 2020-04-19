ddb1a2d429ca52a7bf09143d2975d5540c226e87
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-04-19 10:21:33 +0100

    add missing line of code to update

--- routers/auth.js
+++ routers/auth.js
@@ -60,6 +60,7 @@ router.post("/login", async (req, res) => {
 // Patch User (email/name/password)
 router.patch("/:id", async (req, res) => {
   const id = req.params.id;
+
   const updatedUser = { name: req.body.name, email: req.body.email, passowrd: req.body.password }
 
   try {
@@ -67,13 +68,12 @@ router.patch("/:id", async (req, res) => {
     if (!user) {
       res.status(404).json({ message: "Incorrect User ID" })
     } else {
+      const updated = User.updateOne({ _id: id }, { $set: { updatedUser } })
       res.status(201).json({ message: "Update successful" })
     }
   } catch (err) {
     res.status(500).json({ message: err.message })
   }
-
-
 })
 
 
