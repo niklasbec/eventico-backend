ef0c80f5a1b4b2859978a6dc199d2a0f1051e4bd
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-04-19 10:41:11 +0100

    Add catch to login

--- routers/auth.js
+++ routers/auth.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken")
 
 //import validation
 const { registerValidation, loginValidation } = require("../validation/validation")
+const verify = require("./verifyToken");
 
 router.post("/register", async (req, res) => {
 
@@ -36,39 +37,43 @@ router.post("/register", async (req, res) => {
 
 // Login
 router.post("/login", async (req, res) => {
+
   //validation with joi
   const { error } = loginValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
+  try {
 
-  //Check if email is in system
-  const user = await User.findOne({ email: req.body.email })
-  if (!user) return res.status(400).send("Email doesn't exist!")
+    //Check if email is in system
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send("Email doesn't exist!")
 
-  //Password check
-  const validPass = await bcrypt.compare(req.body.password, user.password)
-  if (!validPass) return res.status(400).send("Invalid password")
+    //Password check
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send("Invalid password")
 
-  //Create JWT
-  const token = jwt.sign({ _id: user.id, name: user.name, email: user.email /*Data we want to provide frontend with*/ }, process.env.JWT_SECRET)
-  res.header("jwt-token", token).send(token)
+    //Create JWT
+    const token = jwt.sign({ _id: user.id, name: user.name, email: user.email /*Data we want to provide frontend with*/ }, process.env.JWT_SECRET)
+    res.header("jwt-token", token).send(token)
 
 
-  res.send("Successfully logged in")
+    res.send("Successfully logged in")
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 
 })
 
 // Patch User (email/name/password)
-router.patch("/:id", async (req, res) => {
-  const id = req.params.id;
+router.patch("/:id", verify, async (req, res) => {
 
   const updatedUser = { name: req.body.name, email: req.body.email, passowrd: req.body.password }
 
   try {
-    const user = await User.findOne({ _id: id })
+    const user = await User.findOne({ _id: req.params.id })
     if (!user) {
       res.status(404).json({ message: "Incorrect User ID" })
     } else {
-      const updated = User.updateOne({ _id: id }, { $set: { updatedUser } })
+      const updated = await User.update({ _id: req.params.id }, { $set: { updatedUser } })
       res.status(201).json({ message: "Update successful" })
     }
   } catch (err) {
@@ -79,10 +84,10 @@ router.patch("/:id", async (req, res) => {
 
 // Delete
 
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
+router.delete("/:id", verify, async (req, res) => {
+
   try {
-    const deleted = await User.findOne({ _id: id })
+    const deleted = await User.findOne({ _id: req.params.id })
     res.status(202).json({ message: deleted })
   } catch (err) {
     res.status(500).json({ message: err.message })
